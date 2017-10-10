a81489a93411ed3e8e1e0c5e2df07018e6013014
aMiT <asj8139@g.rit.edu>
2017-10-10 16:10:23 -0400

    completed file handling

--- transformer.py
+++ transformer.py
@@ -14,19 +14,6 @@ STATUS_ENCRYPT = 'encrypt'
 STATUS_DECRYPT = 'decrypt'
 
 
-def init_file():
-    # messages_file_name = input("Please enter messages file name: ")
-    # transformation_file_name = input("Please enter transformation file name: ")
-
-    messages_file_name = "messages.txt"
-    transformation_file_name = "codes.txt"
-
-    messages_file = open(messages_file_name)
-    transformation_file = open(transformation_file_name)
-
-    return messages_file, transformation_file
-
-
 def user_data():
     status = input("Do you want to encrypt or decrypt?").lower()
     if status != STATUS_ENCRYPT and status != STATUS_DECRYPT:
@@ -43,7 +30,7 @@ def rotate(string, exponent):
         for x in range(exponent):
             string = string[length - 1] + string[0:length - 1]
     else:
-        for x in range(exponent,0):
+        for x in range(exponent, 0):
             string = string[1:length] + string[0]
     return string
 
@@ -54,7 +41,7 @@ def rotate_decrypt(string, exponent):
         for x in range(exponent):
             string = string[1:length] + string[0]
     else:
-        for x in range(exponent,0):
+        for x in range(exponent, 0):
             string = string[length - 1] + string[0:length - 1]
     return string
 
@@ -63,47 +50,47 @@ def shift_char(string, index, exponent):
     c = string[index]
     ascii_value = ord(c)
 
-    if exponent >=0:
+    if exponent >= 0:
         for x in range(exponent):
-            ascii_value = ascii_value+1
+            ascii_value = ascii_value + 1
             if ascii_value == 91:
                 ascii_value = 65
     else:
-        for x in range(exponent,0):
+        for x in range(exponent, 0):
             ascii_value = ascii_value - 1
             if ascii_value == 64:
                 ascii_value = 90
-    result = string[0: index] + chr(ascii_value) + string [index + 1: len(string)]
+    result = string[0: index] + chr(ascii_value) + string[index + 1: len(string)]
     return result
 
 
 def shift_char_decrypt(string, index, exponent):
     c = string.find(index)
     ascii_value = ord(c)
-    if exponent >=0:
+    if exponent >= 0:
         for x in range(exponent):
             ascii_value = ascii_value - 1
             if ascii_value == 64:
                 ascii_value = 90
     else:
-        for x in range(exponent,0):
+        for x in range(exponent, 0):
             ascii_value = ascii_value + 1
             if ascii_value == 90:
                 ascii_value = 64
-    result = string[0: index] + chr(ascii_value) + string [index + 1: len(string)]
+    result = string[0: index] + chr(ascii_value) + string[index + 1: len(string)]
     return result
 
 
 def duplicate(string, index, exponent):
     result = string
     if exponent >= 0:
-        result = string[0: index] + (string[index] * exponent) + string [index: len(string)]
+        result = string[0: index] + (string[index] * exponent) + string[index: len(string)]
     return result
 
 
 def swap_letters(string, i, j):
     result = string
-    if i < j :
+    if i < j:
         result = ""
         for x in range(len(string)):
             if x == i:
@@ -124,21 +111,25 @@ def swap_group_of_letters(string, i, j, g):
         result_list = [string[i:i + n] for i in range(0, len(string), n)]
         result_list[i], result_list[j] = result_list[j], result_list[i]
         for x in range(len(result_list)):
-            result = result+result_list[x]
+            result = result + result_list[x]
 
     return result
 
 
 def transform(messages_file, transformation_file, status):
+    for transformation in transformation_file:
+        message = messages_file.readline().strip().upper()
 
-    for line in transformation_file:
-        operation = line.strip()
-        if len(operation)>1 and operation[1] == '(':
-            transformation_list = [operation[2], operation[4],operation[6]]
+        if message is None or message == '':
+            sys.exit(-1)
+
+        operation = transformation.strip().upper()
+        if len(operation) > 1 and operation[1] == '(':
+            transformation_list = [operation[2], operation[4], operation[6]]
             pass
-        else :
+        else:
             transformation_list = operation[1: len(operation)].split(',')
-        select_function(messages_file.readline().upper().strip() , status, line[0] ,transformation_list)
+        select_function(message, status, transformation[0], transformation_list)
 
         print(transformation_list)
     pass
@@ -146,19 +137,19 @@ def transform(messages_file, transformation_file, status):
 
 def select_function(message, status, operation, transformation_list):
     result = ""
-    if len(transformation_list) == 1 and transformation_list[0] !='':
+    if len(transformation_list) == 1 and transformation_list[0] != '':
         index = int(transformation_list[0])
         exponent = 1
         group = 0
-    elif (len(transformation_list) == 2):
+    elif len(transformation_list) == 2:
         index = int(transformation_list[0])
         exponent = int(transformation_list[1])
         group = 0
-    elif (len(transformation_list) == 3):
+    elif len(transformation_list) == 3:
         group = int(transformation_list[0])
         index = int(transformation_list[1])
         exponent = int(transformation_list[2])
-    else :
+    else:
         index = 0
         exponent = 1
         group = 0
@@ -175,7 +166,7 @@ def select_function(message, status, operation, transformation_list):
     elif operation == 'T':
         if group == 0:
             result = swap_letters(message, index, exponent)
-        else :
+        else:
             result = swap_group_of_letters(message, index, exponent, group)
 
     print(result)
@@ -184,9 +175,35 @@ def select_function(message, status, operation, transformation_list):
 
 
 def main():
-    messages_file, transformation_file = init_file()
-    status = user_data()
-    transform(messages_file, transformation_file, status)
+    messages_file = None
+    transformation_file = None
+    try:
+        # messages_file_name = input("Please enter messages file name: ")
+        # transformation_file_name = input("Please enter transformation file name: ")
+        # status = user_data()
+        messages_file_name = "messages.txt"
+        transformation_file_name = "codes.txt"
+        status = "encrypt"
+
+        messages_file = open(messages_file_name)
+        transformation_file = open(transformation_file_name)
+
+        transform(messages_file, transformation_file, status)
+
+    except FileNotFoundError:
+        print("File not found. Please enter filename properly.")
+
+    except IOError:
+        print("Error in reading files. Please check the files.")
+
+    except:
+        print("Error while executing.")
+
+    finally:
+        if messages_file is not None:
+            messages_file.close()
+        if transformation_file is not None:
+            transformation_file.close()
     pass
 
 
