a5f9c631879b0e15291ce1964d85436e5559c992
aditi <ass7436@rit.edu>
2017-10-31 22:47:58 -0400

    structure done for genetester

--- genetester.py
+++ genetester.py
@@ -1,9 +1,53 @@
 from dnalist import DNAList
 
-def test():
-    list = DNAList()
+def test_init():
 
-    # print_list()
+
+    # dna = DNAList(None)
+    # print(dna)
+    dna = DNAList("")
+    print(dna)
+
+    dna = DNAList("AMit")
+    print(dna)
+
+    dna = DNAList("A")
+    print(dna)
+
+    dna = DNAList("@")
+    print(dna)
+
+    dna = DNAList()
+    print(dna)
+
+    dna = DNAList("YOUTUBE")
+    print(dna)
+
+
+def test_append():
+    dnaAppend = DNAList()
+    print(dnaAppend)
+    dnaAppend.append("A")
+    print(dnaAppend)
+    dnaAppend.append("Aditi")
+    print(dnaAppend)
+    # dnaAppend.append(8)
+    # print(dnaAppend)
+    dnaAppend.append('@')
+    print(dnaAppend)
+    # dnaAppend.append(None)
+    # print(dnaAppend)
+
+
+def test_join():
+    a = 0
+
+
+
+def test():
+    test_init()
+    test_append()
+    test_join()
 
 
 if __name__ == "__main__":
