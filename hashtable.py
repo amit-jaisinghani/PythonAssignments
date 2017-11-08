6bc60a42399b143ecce9e19f37d59579759dc1dc
aditi <ass7436@rit.edu>
2017-11-07 22:57:16 -0500

    Hashtable committing done

--- hashtable.py
+++ hashtable.py
@@ -1,6 +1,17 @@
 from hashmap import Hashmap
 import re
 
+__author__ = 'asj8139,ass7436'
+
+"""
+Assignment 7: Hashing
+Author: Amit Shyam Jaisinghani, Aditi Shailendra Singhai
+
+This program is a testing environment for  testing the
+two hash functions as well as Python's built-in
+hashfunction over three different maximum loadfactors
+and over three different files.
+"""
 
 def add_word(hash_map, word):
     '''
@@ -76,11 +87,18 @@ def generate_stats(file_name):
 
 
 def main():
-    # generate_stats("The beginnings of nature control.txt")
+    """
+    The main function.
+
+    tests the file using different hash function and max loads.
+
+    :return: None
+    """
+    generate_stats("The beginnings of nature control.txt")
 
     # generate_stats("The Family on Wheels.txt")
 
-    generate_stats("dictionary.txt")
+    # generate_stats("dictionary.txt")
     pass
 
 
