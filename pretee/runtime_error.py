c9864752306bbabe7d62c76918f90f8b41a13761
aMiT <asj8139@g.rit.edu>
2017-11-27 20:13:53 -0500

    Removed lab 8

--- pretee/runtime_error.py
+++
@@ -1,9 +0,0 @@
-"""
-CSCI-603 Parser Lab
-Author: Sean Strout @ RIT CS
-
-A custom exception class for representing a runtime error.
-"""
-
-class RuntimeError(Exception):
-    pass
--- pretee/syntax_error.py
+++
@@ -1,9 +0,0 @@
-"""
-CSCI-603 Parser Lab
-Author: Sean Strout @ RIT CS
-
-A custom exception class for representing a syntax error.
-"""
-
-class SyntaxError(Exception):
-    pass
